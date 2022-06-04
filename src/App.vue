c77b746e8d9255551b9e485c5a7cab539f718539
Zmardoc <43533004+Zmardoc@users.noreply.github.com>
2022-06-04 12:54:46 +0200

    init/creating-project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import AppLayout from './components/AppLayout.vue';
+</script>
+
+<template>
+  <app-layout class="full-heigh"/>
+</template>
+
+<style>
+html, body, #app {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: Monospace, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fffb00;
+}
+
+.full-height {
+  height: 100%;
+}
+</style>
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app-layout full-height">
+    Knock knock, Neo...
+  </div>
+</template>
+
+<style scoped>
+  .app-layout {
+    width: 100%;
+    background-color: #131313;
+  }
+</style>
